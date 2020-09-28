55673104d48156f6d4ac904e8ef59cc0ba7cbefe
CloudJay <362359635@qq.com>
2020-09-28 16:47:53 +0800

    增加人员列表

--- src/main/webapp/app/role/list/list.component.ts
+++ src/main/webapp/app/role/list/list.component.ts
@@ -1,8 +1,10 @@
 import Component from 'vue-class-component';
 import { Inject, Vue } from 'vue-property-decorator';
-import { Tree, Row, Col, Button, PageHeader, Form, FormItem, Input } from 'element-ui';
+import { Table, TableColumn, Row, Col, Button, PageHeader, Form, FormItem, Input } from 'element-ui';
 @Component({
   components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
     [Row.name]: Row,
     [Col.name]: Col,
     [PageHeader.name]: PageHeader,
@@ -10,10 +12,48 @@ import { Tree, Row, Col, Button, PageHeader, Form, FormItem, Input } from 'eleme
     [Form.name]: Form,
     [FormItem.name]: FormItem,
     [Input.name]: Input,
-    [Tree.name]: Tree,
   },
   data() {
-    return {};
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333,
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1517 弄',
+          zip: 200333,
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1519 弄',
+          zip: 200333,
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1516 弄',
+          zip: 200333,
+        },
+      ],
+    };
   },
 })
-export default class List extends Vue {}
+export default class List extends Vue {
+  handleClick(row) {
+    console.log(row);
+  }
+}
--- src/main/webapp/app/role/list/list.vue
+++ src/main/webapp/app/role/list/list.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="list-wrap">
-list
-
-    </div>
+  <div class="list-wrap">
+    <el-table :data="tableData" style="width: 100%;">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script lang="ts" src="./list.component.ts"></script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
